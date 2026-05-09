411a81d64c5823a9396f5cef3da06fa8b856ca9b
Jeinne Prado <jeinnejuliane@hotmail.com>
2026-05-09 08:56:25 -0300

    convert numerical grade to letter value

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -3,7 +3,61 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
+//---------Ask the user for the grade ---------
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+
+//---------Convert the text to a number-------
+        string userInput = Console.ReadLine();
+
+//--------Variable to store the letter grade------
+
+        int grade = int.Parse(userInput);
+
+
+// -------Variable to store the letter grade----
+        string letter = "";
+
+//-----------defining the notes in letters-----------
+
+// -----------first part-------------
+        if (grade >=90)
+        {
+            letter = "A";
+        }
+
+// -----------second part-------------
+        else if (grade >=80)
+        {
+        letter = "B";
+        }
+// -----------third part-------------
+        else if (grade >= 70)
+        {
+        letter = "C";
+        }
+// -----------part four-------------
+        else if (grade >= 60)
+        {
+        letter = "D";
+        }
+// -----------fifth part-------------
+        else
+        {
+        letter = "F";
+        }
+//--------Display the letter grade-----
+        Console.WriteLine($"Your grade is {letter}");
+
+//-------Determine if the student passed-------
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying! You can do better next time.");
+        }
     }
 }
