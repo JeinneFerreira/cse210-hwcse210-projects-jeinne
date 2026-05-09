b4a569398b0ebd5c1fbd8117ebf00b1decabb963
Jeinne Prado <jeinnejuliane@hotmail.com>
2026-05-09 18:14:50 -0300

    calculation

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+//----------Ask for numbers----------------
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+//-----------------Calculate sum--------------------
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+//--------------Calculate average-------------
+        double average = (double)sum / numbers.Count;
+
+//----------------Find largest number------------
+        int largest = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+//--------------Display results-------------
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
