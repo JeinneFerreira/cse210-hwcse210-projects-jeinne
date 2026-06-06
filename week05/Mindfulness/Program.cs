ebc31a403cf47e5f0b8c3d390c975f73db82d4e6
Jeinne Prado <jeinnejuliane@hotmail.com>
2026-06-06 19:10:15 -0300

    project completion

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -2,8 +2,56 @@ using System;
 
 class Program
 {
+// ---------------------- Main Program ----------------------
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        int choice = 0;
+
+// ------------------- Continue until user chooses Quit -----------------
+
+        while (choice != 4)
+        {
+            Console.Clear();
+
+            // Display menu
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start Breathing Activity");
+            Console.WriteLine("  2. Start Reflection Activity");
+            Console.WriteLine("  3. Start Listing Activity");
+            Console.WriteLine("  4. Quit");
+            Console.Write("Select a choice from the menu: ");
+
+            choice = int.Parse(Console.ReadLine());
+
+// ---------------------- Execute selected activity ----------------------
+
+
+            switch (choice)
+            {
+                case 1:
+                    BreathingActivity breathing = new BreathingActivity();
+                    breathing.Run();
+                    break;
+
+                case 2:
+                    ReflectionActivity reflection = new ReflectionActivity();
+                    reflection.Run();
+                    break;
+
+                case 3:
+                    ListingActivity listing = new ListingActivity();
+                    listing.Run();
+                    break;
+            }
+
+// ---------------------- Pause before returning to menu ----------------------
+
+            if (choice != 4)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Press Enter to continue...");
+                Console.ReadLine();
+            }
+        }
     }
 }
